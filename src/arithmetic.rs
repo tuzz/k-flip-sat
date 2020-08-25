d38ce69be5c3297f9ec2f8e9d7f8197a41b7c627
Chris Patuzzo <chris@patuzzo.co.uk>
2020-08-25 22:35:37 +0100

    Don’t produce unnecessary bits during counting

--- src/arithmetic.rs
+++ src/arithmetic.rs
@@ -1,34 +1,38 @@
 use crate::*;
 
 pub fn counter(bits: &[i32]) -> Vec<i32> {
+    let max_bits = log2_ceil(bits.len());
+
     let sums = bits.chunks(2).map(|pair| {
         let (sum, carry) = half_adder(pair[0], pair[1]);
         vec![sum, carry]
     }).collect::<Vec<_>>();
 
-    recursive_adder(&sums)
+    recursive_adder(&sums, max_bits)
 }
 
-pub fn recursive_adder(numbers: &[Vec<i32>]) -> Vec<i32> {
+// Implement the parallel-counter encoding described in this paper:
+// https://arxiv.org/pdf/2005.06274.pdf
+// TODO: This currently uses propagation complete adders but I'm not sure it should.
+pub fn recursive_adder(numbers: &[Vec<i32>], max_bits: usize) -> Vec<i32> {
     match numbers.len() {
         1 => numbers[0].clone(),
-        2 => ripple_carry_adder(&numbers[0], &numbers[1]),
+        2 => ripple_carry_adder(&numbers[0], &numbers[1], max_bits),
         n => {
-            let half = round_up_to_power_of_2(n as f32 / 2.);
+            let half = round_up_to_power_of_2(n / 2);
 
-            let left = recursive_adder(&numbers[0..half]);
-            let right = recursive_adder(&numbers[half..]);
+            let left = recursive_adder(&numbers[0..half], max_bits);
+            let right = recursive_adder(&numbers[half..], max_bits);
 
-            let left = pad_with_zeros(&left, right.len());
-            let right = pad_with_zeros(&right, left.len());
-
-            ripple_carry_adder(&left, &right)
+            ripple_carry_adder(&left, &right, max_bits)
         }
     }
 }
 
-// TODO: max bits
-pub fn ripple_carry_adder(a: &[i32], b: &[i32]) -> Vec<i32> {
+pub fn ripple_carry_adder(a: &[i32], b: &[i32], max_bits: usize) -> Vec<i32> {
+    let a = &pad_with_zeros(a, b.len());
+    let b = &pad_with_zeros(b, a.len());
+
     let mut out = vec![];
     let mut c_out = SOLVER.false_literal();
 
@@ -39,7 +43,12 @@ pub fn ripple_carry_adder(a: &[i32], b: &[i32]) -> Vec<i32> {
         c_out = carry;
     }
 
-    out.push(c_out);
+    if out.len() < max_bits {
+        out.push(c_out);
+    } else {
+        SOLVER.add_clause(&[-c_out]);
+    }
+
     out
 }
 
@@ -79,10 +88,18 @@ pub fn half_adder(a: i32, b: i32) -> (i32, i32) {
     (out, c_out)
 }
 
-fn round_up_to_power_of_2(n: f32) -> usize {
-    let mut power = 1.;
-    while power < n { power *= 2.; }
-    power as usize
+fn round_up_to_power_of_2(n: usize) -> usize {
+    2_usize.pow(log2_ceil(n) as u32)
+}
+
+fn log2_ceil(n: usize) -> usize {
+    for i in (0..) {
+        if 2_usize.pow(i) >= n {
+            return i as usize;
+        }
+    }
+
+    unreachable!();
 }
 
 fn pad_with_zeros(bits: &[i32], len: usize) -> Vec<i32> {
@@ -158,12 +175,22 @@ mod test {
     fn it_adds_n_bits() {
         let t = SOLVER.true_literal();
         let f = SOLVER.false_literal();
+        let max = usize::MAX;
 
-        let out = ripple_carry_adder(&[t, t, f, f], &[t, f, f, t]); // 3 + 9
+        let out = ripple_carry_adder(&[t, t, f, f], &[t, f, f, t], max); // 3 + 9
         assert_assigned(&out, &[false, false, true, true, false]); // 12
 
-        let out = ripple_carry_adder(&[f, t, f, t], &[f, t, t, f]); // 10 + 6
+        let out = ripple_carry_adder(&[f, t, f, t], &[f, t, t, f], max); // 10 + 6
         assert_assigned(&out, &[false, false, false, false, true]); // 16
+    }
+
+    #[test]
+    fn it_can_enforce_the_maximum_number_of_bits_returned() {
+        let t = SOLVER.true_literal();
+        let f = SOLVER.false_literal();
+
+        let out = ripple_carry_adder(&[t, t, f, f], &[t, f, f, t], 4); // 3 + 9
+        assert_assigned(&out, &[false, false, true, true]); // 12
     }
 
     #[test]
@@ -176,7 +203,7 @@ mod test {
             vec![t, f, f, t], // 9
             vec![f, t, f, t], // 10
             vec![f, t, t, f], // 6
-        ]);
+        ], usize::MAX);
 
         assert_assigned(&out, &[false, false, true, true, true, false]); // 28
     }
@@ -187,12 +214,12 @@ mod test {
         let f = SOLVER.false_literal();
 
         let out = counter(&[f, f, f, f, f, f, f, f, f, f]);
-        assert_assigned(&out, &[false, false, false, false, false]); // 0
+        assert_assigned(&out, &[false, false, false, false]); // 0
 
         let out = counter(&[t, f, t, f, t, t, f, t, f, f]);
-        assert_assigned(&out, &[true, false, true, false, false]); // 5
+        assert_assigned(&out, &[true, false, true, false]); // 5
 
         let out = counter(&[t, t, t, t, t, t, t, t, t, t]);
-        assert_assigned(&out, &[false, true, false, true, false]); // 10
+        assert_assigned(&out, &[false, true, false, true]); // 10
     }
 }
