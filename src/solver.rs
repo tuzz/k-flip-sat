28886852a360f86fdb4d3025c56678064536424c
Chris Patuzzo <chris@patuzzo.co.uk>
2020-08-26 01:09:17 +0100

    Refactor: wrap all Solver fields in RefCell

--- src/solver.rs
+++ src/solver.rs
@@ -5,9 +5,13 @@ lazy_static! {
 }
 
 pub struct Solver {
+    inner: Rc<RefCell<Inner>>,
+}
+
+pub struct Inner {
     pointer: *mut std::ffi::c_void,
-    literals: RefCell<u32>,
-    clauses: RefCell<u32>,
+    literals: u32,
+    clauses: u32,
     true_literal: i32,
     false_literal: i32,
 }
@@ -16,29 +20,39 @@ const LOG_DIMACS: bool = false;
 
 impl Solver {
     pub fn new() -> Self {
-        let mut solver = Self {
+        let inner = Inner {
             pointer: unsafe { ipasir_init() },
-            literals: RefCell::new(0),
-            clauses: RefCell::new(0),
+            literals: 0,
+            clauses: 0,
             true_literal: 0,
             false_literal: 0,
         };
 
-        solver.true_literal = solver.new_literal();
-        solver.false_literal = solver.new_literal();
+        let solver = Self { inner: Rc::new(RefCell::new(inner)) };
 
-        solver.add(solver.true_literal);
+        let t = solver.new_literal();
+        let f = solver.new_literal();
+
+        solver.add(t);
         solver.add(0);
 
-        solver.add(-solver.false_literal);
+        solver.add(-f);
         solver.add(0);
 
+        let mut inner = solver.inner.borrow_mut();
+
+        inner.true_literal = t;
+        inner.false_literal = f;
+
+        drop(inner);
         solver
     }
 
     pub fn new_literal(&self) -> i32 {
-        *self.literals.borrow_mut() += 1;
-        *self.literals.borrow() as i32
+        let mut inner = self.inner.borrow_mut();
+
+        inner.literals += 1;
+        inner.literals as i32
     }
 
     pub fn true_literal(&self) -> i32 {
@@ -51,7 +65,10 @@ impl Solver {
 
     pub fn add(&self, literal: i32) {
         unsafe { ipasir_add(self.pointer, literal); }
-        if literal == 0 { *self.clauses.borrow_mut() += 1; }
+
+        if literal == 0 {
+            self.inner.borrow_mut().clauses += 1;
+        }
 
         if LOG_DIMACS {
             print!("{} ", literal);
@@ -78,19 +95,19 @@ impl Solver {
     pub fn assignment(&self, literal: i32) -> bool {
         unsafe { ipasir_val(self.pointer, literal) == literal }
     }
-
-    pub fn literals(&self) -> u32 {
-        *self.literals.borrow()
-    }
-
-    pub fn clauses(&self) -> u32 {
-        *self.clauses.borrow()
-    }
 }
 
 impl Drop for Solver {
     fn drop(&mut self) {
         unsafe { ipasir_release(self.pointer); }
+    }
+}
+
+impl Deref for Solver {
+    type Target = Inner;
+
+    fn deref(&self) -> &Self::Target {
+        unsafe { self.inner.try_borrow_unguarded().unwrap() }
     }
 }
 
